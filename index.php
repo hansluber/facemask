3ba5d91ea58e8c20a80d89f85c6c2bc554be5f5f
marthansluber <mart.hans.luber@gmail.com>
2021-01-01 16:00:28 +0700

    initate

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>faceMask</title>
+     <link rel="stylesheet" href="css/facemask.min.css" />
+     <link rel="stylesheet" href="css/bootstrap.min.css" />
+</head>
+<body class="bg-light">
+    <header>
+        <div class="jumbotron jumbotron-fluid ">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-9">
+                        <img class="facemaskLogo img-responsive" src="img/facemaskLogo.png" />
+                        <!-- <h1>Selamat datang di Facemask</h1> -->
+                        <p>Facemask membantu Anda terhubung dan berbagi dengan </br>orang-orang bertopeng dalam kehidupan Anda</p>
+                    </div>
+                    <div class="col-md-2">
+                        <a href="login.php" class="btn btn-primary">Masuk</a>
+                        <a href="register.php" class="btn btn-success">Daftar</a>
+                        <!-- <div class="card-group">
+                            <div class="card">
+                                <img class="login" src="img/loginFace.png" alt="Card image cap">
+                                <div class="card-body" >
+                                <h5 class="card-title">Masuk</h5>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <img class="register" src="img/registerFace.png" alt="Card image cap">
+                                <div class="card-body">
+                                <h5 class="card-title">Daftar</h5>
+                                </div>
+                            </div>
+                        </div> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section>
+        <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <img class="faceMaskIdentity img-responsive" src="img/faceMask.png" />
+                    </div>
+                </div>
+            </div>
+    </section>
+
+</body>
+</html>
